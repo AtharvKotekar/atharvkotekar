ae843322c44cd98f6ac0bc9b001632f1181d8ed9
Atharv Kotekar <89241587+AtharvKotekar@users.noreply.github.com>
2025-07-27 18:00:55 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,8 @@ const Home = () => {
     <div className="w-screen h-screen justify-center items-center flex flex-col">
       <h1 className="text-5xl font-bold text-black px-8 text-center">hii! i'm atharv kotekar.</h1>
       <h2 className="text-xl max-w-4xl text-center py-6 px-8">
-        18; building cool stuff; and not just an another vibe coder. currently making <a href="https://www.opendoc.pro" target="_blank" className="underline">@opendoc.</a><br /> 
-        i’m super active on <a href="https://x.com/atharv_kotekar" target="_blank" className="underline">𝕏</a>, <a href="https://www.instagram.com/_atharvkotekar/" target="_blank" className="underline">instagram</a>, and this <a href="https://www.linkedin.com/in/atharvkotekar/" target="_blank" className="underline">“thrilled to announce platform.”</a> 
+        18; building cool stuff; and not just an another vibe coder.<br /> 
+        i’m super active on <a href="https://x.com/_atharvkotekar" target="_blank" className="underline">𝕏</a>, <a href="https://www.instagram.com/_atharvkotekar/" target="_blank" className="underline">instagram</a>, and this <a href="https://www.linkedin.com/in/atharvkotekar/" target="_blank" className="underline">“thrilled to announce platform.”</a> 
         <br />
       </h2>
 
